65d1197d824012f8c093056a259d162272bea50c
A. Thompson <thompson@tamu.edu>
2021-08-24 15:20:41 -0500

    moving source code out of src/

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
-# init alplib
+# initialize src
+
+from .constants import *
+from .borrmann import Borrmann
+from .crystal import *
+from .decay import *
+
+
+__all__ = ['Borrmann', 'Crystal', 'get_crystal']
--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# initialize src
-
-from .constants import *
-from .borrmann import Borrmann
-from .crystal import *
-from .decay import *
-
-
-__all__ = ['Borrmann', 'Crystal', 'get_crystal']
