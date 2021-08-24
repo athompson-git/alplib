7a0c3bf62259daa2efc05a660b051be55c4bd24d
A. Thompson <thompson@tamu.edu>
2021-08-24 12:52:28 -0500

    fixing typo in DFSZ coupling

--- src/qcd_axion.py
+++ src/qcd_axion.py
@@ -15,17 +15,25 @@ def Cae(ma, tanbeta, dfsz_type):  # ma in eV
     fa = 5.7e6 / ma
     if dfsz_type == "DFSZI":
         EbyN = 8/3
-        return -(1/3)*sin(arctan(tanbeta))**2 + (3*alpha**2)/(4*pi**2) * (EbyN * log(fa/(0.511e-3)) - 1.92 * log(1/(0.511e-3)))
+        return (1/3)*sin(arctan(tanbeta))**2 + (3*alpha**2)/(4*pi**2) * (EbyN * log(fa/(0.511e-3)) - 1.92 * log(1/(0.511e-3)))
     if dfsz_type == "DFSZII":
         EbyN = 2/3
-        return (1/3)*cos(arctan(tanbeta))**2 + (3*alpha**2)/(4*pi**2) * (EbyN * log(fa/(0.511e-3)) - 1.92 * log(1/(0.511e-3)))
+        return -(1/3)*cos(arctan(tanbeta))**2 + (3*alpha**2)/(4*pi**2) * (EbyN * log(fa/(0.511e-3)) - 1.92 * log(1/(0.511e-3)))
 
 
 
 
-def gae_DFSZ(ma, tanbeta, dfsz_type):  # ma in eV
+def f_a(ma):
+    # ma in eV, returns f_a in GeV
+    return 5.691e6 / ma
+
+
+
+
+def gae_DFSZ(ma, tanbeta, dfsz_type):
+    # ma in eV
     # return g_ae as a function of m_a, \tan\beta, and the DFSZ model (I or II)
-    return abs(1.8e-7 * (0.511e-3) * ma * Cae(ma, tanbeta, dfsz_type))
+    return abs((M_E*1e-3) * Cae(ma, tanbeta, dfsz_type) / f_a(ma))
 
 
 
@@ -39,10 +47,12 @@ def gagamma_KSVZ(ma, eByN):
 
 
 def gagamma_DFSZI(ma):
+    # ma in eV
     return (0.203*8/3 - 0.39)*ma*1e-9
 
 
 
 
 def gagamma_DFSZII(ma):
+    # ma in eV
     return (0.203*2/3 - 0.39)*ma*1e-9
