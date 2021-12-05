61e23899ab04292f3a185f9982c3a155303c8072
A. Thompson <thompson@tamu.edu>
2021-12-05 11:30:43 -0800

    added mineral oil

--- photon_xs.py
+++ photon_xs.py
@@ -29,6 +29,8 @@ class AbsCrossSection:
             self.xs_dim = 149.89 / AVOGADRO  # (cm2 / g  * g / mol  * mol / N)
         elif self.mat_name == "CsI":
             self.xs_dim = 259.81 / AVOGADRO  # (cm2 / g  * g / mol  * mol / N)
+        elif self.mat_name == "CH2":
+            self.xs_dim = 259.81 / AVOGADRO  # (cm2 / g  * g / mol  * mol / N)
         
         self.cleanPEData()
     
