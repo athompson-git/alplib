5b83c49e7260d1a099db56d9b226e35efed913b4
A. Thompson <thompson@physics.tamu.edu>
2022-08-10 20:47:16 -0500

    added FluxPairAnnihilationGamma for e+e- -> a gamma, added more volume integration methods

--- fluxes.py
+++ fluxes.py
@@ -147,6 +147,13 @@ class FluxPrimakoffIsotropic(AxionFlux):
             geom_accept = self.det_area / (4*pi*self.det_dist**2)
             self.decay_axion_weight *= geom_accept
             self.scatter_axion_weight *= geom_accept
+    
+    def propagate_iso_vol_int(self, geom: DetectorGeometry, new_coupling=None):
+        if new_coupling is not None:
+            rescale=power(new_coupling/self.gagamma, 2)
+            super().propagate_iso_vol_int(geom, W_gg(new_coupling, self.ma), rescale)
+        else:
+            super().propagate_iso_vol_int(geom, W_gg(self.gagamma, self.ma))
 
 
 
@@ -266,7 +273,6 @@ class FluxBremIsotropic(AxionFlux):
         el_wgt = electron[1]
 
         ea_max = el_energy * (1 - power(self.ma/el_energy, 2))
-        #ea_max = el_energy
         if ea_max < self.ma:
             return
 
@@ -284,6 +290,14 @@ class FluxBremIsotropic(AxionFlux):
         self.scatter_axion_weight = []
         self.decay_axion_weight = []
 
+        """
+        ep_min = max((self.ma**2 - M_E**2)/(2*M_E), M_E)
+        for i, el in enumerate(self.electron_flux):
+            t_depth = np.random.uniform(0.0, 5.0)
+            new_energy = np.random.uniform(ep_min, el[0])
+            flux_weight = self.electron_flux_attenuated(t_depth, el[0], new_energy) * 5.0 * (el[0] - ep_min)
+            self.simulate_single([new_energy, flux_weight])
+        """
         for i, el in enumerate(self.electron_flux):
             self.simulate_single(el)
     
@@ -826,3 +840,93 @@ class FluxPi0Isotropic(AxionFlux):
             self.decay_axion_weight = np.asarray(wgt*0.0, dtype=np.float64)
             self.scatter_axion_weight = np.asarray(wgt, dtype=np.float64)
 
+
+
+
+
+class FluxPairAnnihilationGamma(AxionFlux):
+    """
+    Generator associated production via electron-positron annihilation into gamma + ALP
+    (e+ e- -> a gamma) via virtual photon
+    Takes in a flux of positrons
+    """
+    def __init__(self, positron_flux=[1.,0.], target=Material("W"),
+                 target_radiation_length=6.76, det_dist=4., det_length=0.2, det_area=0.04,
+                 axion_mass=0.1, axion_coupling=1e-3, n_samples=100, is_isotropic=True):
+        super().__init__(axion_mass, target, det_dist, det_length, det_area, n_samples)
+        self.positron_flux = positron_flux  # differential positron energy flux dR / dE+ / s
+        self.positron_flux_bin_widths = positron_flux[1:,0] - positron_flux[:-1,0]
+        self.positron_flux_bin_centers = (positron_flux[1:,0] + positron_flux[:-1,0])/2
+        self.gagamma = axion_coupling
+        self.ntarget_area_density = target_radiation_length * AVOGADRO / (2*target.z[0])  # todo: change 2*Z
+        self.is_isotropic = is_isotropic
+        self.positron_flux_smeared_wgts = []
+        self.positron_flux_smeared_energies = []
+    
+    def track_length_prob(self, Ei, Ef, t):
+        b = 4/3
+        return heaviside(Ei-Ef, 0.0) * abs(power(log(Ei/Ef), b*t - 1) / (Ei * gamma(b*t)))
+    
+    def positron_flux_dN_dE(self, energy):
+        return np.interp(energy, self.positron_flux[:,0], self.positron_flux[:,1], left=0.0, right=0.0)
+    
+    def positron_flux_attenuated(self, t, energy_pos, energy_res):
+        return self.positron_flux_dN_dE(energy_pos) * self.track_length_prob(energy_pos, energy_res, t)
+    
+    def simulate_positron_flux(self):
+        self.positron_flux_smeared_energies = []
+        self.positron_flux_smeared_wgts = []
+        ep_min = max((self.ma**2 - M_E**2)/(2*M_E), M_E)
+        print("ep min = ", ep_min)
+        for i in range(self.positron_flux_bin_centers.shape[0]):
+            ep_lab = self.positron_flux_bin_centers[i]
+            if ep_lab < ep_min:
+                continue  # Threshold check
+
+            # Simulate ALPs produced in the CM frame
+            t_depths = np.random.uniform(0.0, 5.0, self.n_samples)
+            smeared_energies = np.random.uniform(ep_min, ep_lab, self.n_samples)
+
+            flux_wgts = self.positron_flux_attenuated(t_depths, ep_lab, smeared_energies)
+            self.positron_flux_smeared_wgts.extend(self.positron_flux_bin_widths[i] * 5.0 * (ep_lab-ep_min) * flux_wgts / self.n_samples)
+            self.positron_flux_smeared_energies.extend(smeared_energies)
+    
+    def simulate(self):
+        self.axion_energy = []
+        self.axion_flux = []
+        self.scatter_axion_weight = []
+        self.decay_axion_weight = []
+
+        self.simulate_positron_flux()
+
+        for i in range(len(self.positron_flux_smeared_wgts)):
+            ep = self.positron_flux_smeared_energies[i]
+            pos_flux = self.positron_flux_smeared_wgts[i]
+            beta = sqrt(ep**2 - M_E**2) / (M_E + ep)
+            gamma = power(1-beta**2, -0.5)
+            s = 2 * M_E * (M_E + ep)
+            ps_cm = sqrt((s - self.ma**2)**2 / (4*s))
+            es_cm = sqrt(ps_cm**2 + self.ma**2)
+
+            es_min = gamma*(es_cm - ps_cm*beta)
+            es_max = gamma*(es_cm + ps_cm*beta)
+            alp_energies = np.random.uniform(es_min, es_max, 1)
+            mc_volume_diffxs = 2 * gamma * beta * ps_cm
+            
+            cm_wgts = (self.ntarget_area_density * HBARC**2) \
+                * epem_to_alp_photon_dsigma_de(alp_energies, ep, self.gagamma, self.ma, self.target_z)
+
+            
+            self.axion_energy.extend(alp_energies)
+            self.axion_flux.extend(cm_wgts * pos_flux * mc_volume_diffxs)
+
+    def propagate(self, new_coupling=None):
+        if new_coupling is not None:
+            super().propagate(W_gg(new_coupling, self.ma), rescale_factor=power(new_coupling/self.gagamma, 2))
+        else:
+            super().propagate(W_gg(self.gagamma, self.ma))
+        
+        if self.is_isotropic:
+            geom_accept = self.det_area / (4*pi*self.det_dist**2)
+            self.decay_axion_weight *= geom_accept
+            self.scatter_axion_weight *= geom_accept
