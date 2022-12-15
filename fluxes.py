corrected jacobian factor

fluxes.py
@@ -617,15 +617,13 @@ class FluxChargedMeson3BodyDecay(AxionFlux):
         pz_lab = boost*(pz + beta*energies)
         cos_theta_lab = pz_lab / sqrt(e_lab**2 - self.ma**2)
 
-        # Jacobian for transforming d2Gamma/(dEa * dOmega) to lab frame:
-        jacobian = sqrt(e_lab**2 - self.ma**2) / momenta
         # Monte Carlo volume, making sure to use the lab frame energy range
         #mc_vol = (ea_max - ea_min)*(1-min_cm_cos)
-        mc_vol_lab = boost*(ea_max - sqrt(ea_max**2 - self.ma**2)*beta) - boost*(ea_min + sqrt(ea_min**2 - self.ma**2)*beta)
+        mc_vol_lab = 2 * boost * beta * sqrt(e_lab**2 - self.ma**2)  # uses jacobian factor
 
         # Draw weights from the PDF
-        weights = np.array([pion_wgt*jacobian*mc_vol_lab*self.dGammadEa(ea, ml)/self.total_width/self.n_samples \
-            for ea in energies])
+        weights = np.array([pion_wgt*mc_vol_lab[i]*self.dGammadEa(energies[i], ml)/self.total_width/self.n_samples \
+            for i in range(energies.shape[0])])
 
         for i in range(self.n_samples):
             solid_angle_acceptance = heaviside(cos_theta_lab[i] - solid_angle_cosine, 0.0)