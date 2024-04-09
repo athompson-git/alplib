9cbb4a2b4a228da80c4200acb3e1ac1c97f74d3a
athompson-tamu <thompson@physics.tamu.edu>
2024-04-09 09:55:32 -0500

    adding expon import

--- fmath.py
+++ fmath.py
@@ -5,8 +5,7 @@ from numpy import log, log10, exp, pi, sqrt, power, \
     sin, cos, tan, arccos, arctan, arcsin, arctan2, heaviside, dot, cross
 from scipy.integrate import quad, dblquad
 from scipy.special import exp1, erf, gamma
-from scipy.stats import norm, chisquare
-
+from scipy.stats import norm, chisquare, expon
 
 
 
@@ -23,20 +22,3 @@ def fastMC2D(func, a, b, c, d, n_samples):
     pass
 
 
-
-
-def lorentz_boost(momentum, v):
-    """
-    Lorentz boost momentum to a new frame with velocity v
-    :param momentum: four vector
-    :param v: velocity of new frame, 3-dimention
-    :return: boosted momentum
-    """
-    n = v/np.sqrt(np.sum(v**2))
-    beta = np.sqrt(np.sum(v**2))
-    gamma = 1/np.sqrt(1-beta**2)
-    mat = np.array([[gamma, -gamma*beta*n[0], -gamma*beta*n[1], -gamma*beta*n[2]],
-                    [-gamma*beta*n[0], 1+(gamma-1)*n[0]*n[0], (gamma-1)*n[0]*n[1], (gamma-1)*n[0]*n[2]],
-                    [-gamma*beta*n[1], (gamma-1)*n[1]*n[0], 1+(gamma-1)*n[1]*n[1], (gamma-1)*n[1]*n[2]],
-                    [-gamma*beta*n[2], (gamma-1)*n[2]*n[0], (gamma-1)*n[2]*n[1], 1+(gamma-1)*n[2]*n[2]]])
-    return mat @ momentum
