4d6771b2209f2052724890822bfe8711b7755201
athompson-tamu <thompson@physics.tamu.edu>
2025-04-20 15:00:16 -0500

    adding nu_integral for track length integration

--- fmath.py
+++ fmath.py
@@ -26,3 +26,11 @@ def fastMC2D(func, a, b, c, d, n_samples):
 
 def kallen_alplib(x, y, z):
     return x*x + y*y + z*z - 2*x*y - 2*y*z - 2*z*x
+
+
+
+
+def nu_integral(x, alpha, T_max=np.inf):
+    f = lambda t: np.power(x, t + alpha) / gamma(t + 1 + alpha)
+
+    return quad(f, 0.0, T_max)[0]
